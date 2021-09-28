a58b8fe34b659392d22f1edb704254c23c918f0c
hueybry <hueybry@urbandaleschools.com>
2021-09-27 21:50:49 -0500

    First Proto trype

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,11 +1,4 @@
-# gas milleage program
-
-miles_driven = float(input("how long was your trip? "))
-gallons_used = float(input("manys how many gallons of gas did you use? "))
-mpg = miles_driven/gallons_used
-print("your gas mileage",mpg,"mpg")
-
-
-
-
-
+# AND/OR
+a=1
+b=2
+c=3
