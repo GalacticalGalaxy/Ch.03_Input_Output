97f8d04938c96373a8de740f752d41488bf4cfa4
hueybry <hueybry@urbandaleschools.com>
2021-10-05 11:40:47 -0500

    .-.

---
+++ .idea/Class chalange.py
@@ -0,0 +1,7 @@
+import turtle
+# side = int(input("How many sides do you want on your polygon?- "))
+for i in range(1+int(input("How many saides"))):
+    print()
+    # ( turtle.right (360/side), turtle.forward(100))
+
+# turtle.exitonclick()
